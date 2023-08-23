4d4d98c5d4fcf31442be7b475c584220481a79cc
Tribulations <joco2201@student.miun.se>
2023-08-23 12:34:01 +0200

    Candlestick.java: added documentation

--- src/main/java/stocker/representation/Candlestick.java
+++ src/main/java/stocker/representation/Candlestick.java
@@ -18,6 +18,9 @@ public class Candlestick {
     //  adding data to the database so we can set the timestamp/date correctly
     private String interval;
 
+    /**
+     * Default public construction initializing all member fields to 0.
+     */
     public Candlestick() {
         this.open = 0;
         this.close = 0;
@@ -37,6 +40,16 @@ public class Candlestick {
 //        this.timestamp = timestamp;
 //    }
 
+    /**
+     * Public construction initializing all member fields.
+     * @param open the candlestick's opening price
+     * @param close the candlestick's closing price
+     * @param low the candlestick's lowest price
+     * @param high the candlestick's highest price
+     * @param volume the total volume of candlestick
+     * @param timestamp the candlestick timestamp
+     * @param interval the interval of the candlestick e.g. 1 day, 1 min 15 min etc.
+     */
     public Candlestick(final double open, final double close, final double low, final double high,
                        final long volume, final long timestamp, final String interval) {
         this.open = open;
@@ -48,45 +61,77 @@ public class Candlestick {
         this.interval = interval;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's opening price
+     */
     public double getOpen() {
         return open;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's closing price
+     */
     public double getClose() {
         return close;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's lowest price
+     */
     public double getLow() {
         return low;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's highest price
+     */
     public double getHigh() {
         return high;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's total volume
+     */
     public long getVolume() {
         return volume;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's timestamp
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Public accessor.
+     * @return the candlestick's interval 1 day, 15 min etc.
+     */
     public String getInterval() {
         return interval;
     }
 
+    /**
+     * Returns the candlestick's timestamp in a more readable way e.g. 2023-08-01 12:05:00.
+     * @return the candlestick's timestamp with the format yyyy-MM-dd HH:mm:ss
+     */
     public String getHumanReadableDate() {
         return DateTimeFormatter.ofPattern(
                 "yyyy-MM-dd HH:mm:ss").format(
                         Instant.ofEpochSecond(timestamp).atZone(ZoneId.systemDefault()));
     }
 
-    public static String asHumanReadableDate(long timestamp) {
-        return DateTimeFormatter.ofPattern(
-                "yyyy-MM-dd HH:mm").format(
-                Instant.ofEpochSecond(timestamp).atZone(ZoneId.systemDefault()));
-    }
+//    public static String asHumanReadableDate(long timestamp) {
+//        return DateTimeFormatter.ofPattern(
+//                "yyyy-MM-dd HH:mm").format(
+//                Instant.ofEpochSecond(timestamp).atZone(ZoneId.systemDefault()));
+//    }
 
 
     public void setOpen(double open) {
