09e3be21f136577c42ffd01c0dc9dcaa2eb3d4de
Tribulations <joco2201@student.miun.se>
2024-06-06 12:39:56 +0200

    Add analyzer packages

---
+++ src/main/java/stocker/analyzers/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * The {@code stocker.analyzers} package provides a suite of tools and utilities
+ * for analyzing stock data. This package includes classes and methods for 
+ * computing various financial indicators, detecting chart patterns, analyzing
+ * candlestick formations, and more. It is designed to facilitate quantitative
+ * analysis and technical trading strategies in stock market analysis.
+ *
+ * <p>The key functionalities provided by this package include:
+ * <ul>
+ *     <li>Calculation of technical indicators such as moving averages, RSI, MACD, etc.</li>
+ *     <li>Detection and analysis of common chart patterns like head and shoulders, double top, etc.</li>
+ *     <li>Candlestick pattern recognition for identifying bullish or bearish trends.</li>
+ *     <li>Customizable analysis frameworks for more advanced and user-defined financial metrics.</li>
+ * </ul>
+ *
+ * @author Joakim Colloz
+ * @version 1.0
+ * @since 2024
+ */
+package stocker.analyzers;
