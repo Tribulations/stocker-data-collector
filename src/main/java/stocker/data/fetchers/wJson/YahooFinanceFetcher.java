cb9d6875ce4e512b87e1ab7f061a571758d035e8
Joakim Colloz <kung_jocke@hotmail.com>
2025-07-05 17:53:02 +0200

    Use environment variables for config and secrets
    - Updated YahooFinanceFetcher and DbConstants to use
      environment variables instead of hardcoded values.
    - Moved SQL query strings to DbConstants.
    - Added .env to .gitignore to avoid committing sensitive data.

--- src/main/java/stocker/data/fetchers/wJson/YahooFinanceFetcher.java
+++ src/main/java/stocker/data/fetchers/wJson/YahooFinanceFetcher.java
@@ -1,16 +1,22 @@
 package stocker.data.fetchers.wJson;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 /**
- *
+ * Yahoo Finance data fetcher that retrieves stock data from the Yahoo Finance API.
  */
 public class YahooFinanceFetcher extends BaseDataFetcher {
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     public static final YahooFinanceFetcher INSTANCE = new YahooFinanceFetcher();
 
-    private YahooFinanceFetcher()  {// TODO add these values as constants
-        super("X-RapidAPI-Key", "X-RapidAPI-Host",
-                "3a491bbe0amshadc3d607a3d7e2dp1c9ee5jsnb82d483841a2",
-                "stock-data-yahoo-finance-alternative.p.rapidapi.com",
-                "https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v8/finance/chart/");
+    private YahooFinanceFetcher() {
+        super(
+            dotenv.get("YAHOO_API_KEY_HEADER"),
+            dotenv.get("YAHOO_API_HOST_HEADER"),
+            dotenv.get("YAHOO_API_KEY"),
+            dotenv.get("YAHOO_API_HOST"),
+            dotenv.get("YAHOO_API_URL")
+        );
     }
 }
 
--- src/main/java/stocker/database/DbConstants.java
+++ src/main/java/stocker/database/DbConstants.java
@@ -1,24 +1,27 @@
 package stocker.database;
 
-import stocker.security.Authenticator;
+import io.github.cdimascio.dotenv.Dotenv;
 
 /**
  * Storing constants used in classes interacting with the database.
  */
 public final class DbConstants {
-
+    
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    
     public DbConstants() {
         throw new IllegalStateException("Utility class");
     }
 
     /** Database connection constants */
-    public static final String DB_IP_ADDRESS = "213.164.214.9";
-    public static final String DB_PORT = ":5432";
-    public static final String DB_NAME = "test_db";
+    public static final String DB_IP_ADDRESS = dotenv.get("DB_IP_ADDRESS", "localhost");
+    public static final String DB_PORT = ":" + dotenv.get("DB_PORT", "5432");
+    public static final String DB_NAME = dotenv.get("DB_NAME", "test_db");
     public static final String DB_URL = "jdbc:postgresql://" + DB_IP_ADDRESS + DB_PORT + "/" + DB_NAME;
-    public static final String DB_USERNAME = Authenticator.INSTANCE.getDbUsername();
-    public static final String DB_PASSWORD = Authenticator.INSTANCE.getDbPassword();
-    public static final String CANDLESTICK_TABLE = "test_schema.stock_prices1";
+    public static final String DB_USERNAME = dotenv.get("DB_USERNAME", "");
+    public static final String DB_PASSWORD = dotenv.get("DB_PASSWORD", "");
+    public static final String CANDLESTICK_TABLE = dotenv.get("DB_SCHEMA", "test_schema") + "." + 
+                                                  dotenv.get("DB_TABLE", "stock_prices1");
 
     /** Database table and attribute name constants */
     public static final String TIMESTAMP_COLUMN = "timestamp";
@@ -27,8 +30,20 @@ public final class DbConstants {
     public static final String LOW_COLUMN = "low";
     public static final String HIGH_COLUMN = "high";
     public static final String VOLUME_COLUMN = "volume";
-    public static final String INTERVAL_COLUMN = "interval";
+    public static final String INTERVAL_COLUMN = "interval"; // TODO remove as not used?
+
+    /** SQL query constants */
     public static final String INSERT_CANDLESTICK_QUERY = "INSERT INTO " + CANDLESTICK_TABLE
             + " (timestamp, open, close, low, high, volume, symbol) "
             + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+    public static final String SELECT_ALL_QUERY = "SELECT * FROM " + CANDLESTICK_TABLE;
+    public static final String SELECT_BY_SYMBOL_QUERY = "SELECT * FROM " + CANDLESTICK_TABLE + " WHERE symbol = ?";
+    public static final String INSERT_ON_CONFLICT_UPDATE_QUERY = "INSERT INTO " + CANDLESTICK_TABLE
+            + " (timestamp, open, close, low, high, volume, symbol, interval) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT (time_stamp, symbol) DO UPDATE SET "
+            + "(open, close, low, high, volume) = (excluded.open, excluded.close, excluded.low, excluded.high, excluded.volume)";
+    public static final String INSERT_ON_CONFLICT_DO_NOTHING_QUERY = "INSERT INTO " + CANDLESTICK_TABLE
+            + " (timestamp, open, close, low, high, volume, symbol, interval) "
+            + "VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING";
+    public static final String RESET_TABLE_QUERY = "TRUNCATE TABLE " + CANDLESTICK_TABLE;
 }
