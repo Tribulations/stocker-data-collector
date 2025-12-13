dffcb852158a7c4edf02a157623892211b893ac3
Joakim Colloz <kung_jocke@hotmail.com>
2025-12-13 12:47:29 +0100

    Add logic to main() method
    Fetch data (1 day range/interval) and insert into database

--- src/main/java/com/joakimcolloz/stocker/datacollector/Main.java
+++ src/main/java/com/joakimcolloz/stocker/datacollector/Main.java
@@ -1,5 +1,15 @@
 package com.joakimcolloz.stocker.datacollector;
 
+import com.joakimcolloz.stocker.datacollector.data.StockDataService;
+import com.joakimcolloz.stocker.datacollector.database.DatabaseConfig;
+import com.joakimcolloz.stocker.datacollector.database.DatabaseManager;
+import com.joakimcolloz.stocker.datacollector.model.Interval;
+import com.joakimcolloz.stocker.datacollector.model.Range;
+import com.joakimcolloz.stocker.datacollector.utils.StockReader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Main class of the program.
  *
@@ -8,5 +18,19 @@ package com.joakimcolloz.stocker.datacollector;
  */
 public class Main {
     public static void main(String... args) {
+        final DatabaseManager databaseManager = new DatabaseManager(new DatabaseConfig());
+        databaseManager.initialize();
+
+        ArrayList<String> stockList;
+
+        final StockDataService stockDataService = new StockDataService();
+        try {
+            stockList = StockReader.readStockNamesFromResource("largecap.txt");
+        } catch (IOException e) {
+            System.out.println(e.getMessage()); // TODO temp logging improve!
+            throw new RuntimeException(e);
+        }
+
+        stockDataService.addPriceDataToDb(stockList, Range.ONE_DAY, Interval.ONE_DAY);
     }
 }
