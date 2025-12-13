93da59c858db9e02cbf037b9503af24e4f501482
Joakim Colloz <kung_jocke@hotmail.com>
2025-12-13 12:26:33 +0100

    Improve comments of candlestick properties in sql file

--- src/main/resources/db/migration/V001__Create_stock_prices_table.sql
+++ src/main/resources/db/migration/V001__Create_stock_prices_table.sql
@@ -14,11 +14,11 @@ CREATE TABLE IF NOT EXISTS stock_prices_schema.stock_prices_1day (
 
 -- Comments for documentation
 COMMENT ON TABLE stock_prices_schema.stock_prices_1day IS '1-day Stock price candlestick data';
-COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.timestamp IS 'Unix timestamp of the candlestick period';
+COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.timestamp IS 'Unix timestamp marking the start of the candlestick interval';
 COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.open IS 'Opening price';
 COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.close IS 'Closing price';
-COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.low IS 'Lowest price during period';
-COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.high IS 'Highest price during period';
+COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.low IS 'Lowest price during interval';
+COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.high IS 'Highest price during interval';
 COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.volume IS 'Trading volume';
 COMMENT ON COLUMN stock_prices_schema.stock_prices_1day.symbol IS 'Stock symbol (e.g., BOL.ST, ABB.ST)';
 
