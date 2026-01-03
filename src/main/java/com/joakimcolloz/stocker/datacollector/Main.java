a767851f36c499280bf9ec48807414fc693cbce2
Joakim Colloz <kung_jocke@hotmail.com>
2026-01-03 11:26:56 +0100

    Allow setting used api with cli argument
    - Specify which API to use via CLI argument
    - Added logging
    - Improved error logging

--- src/main/java/com/joakimcolloz/stocker/datacollector/Main.java
+++ src/main/java/com/joakimcolloz/stocker/datacollector/Main.java
@@ -2,12 +2,16 @@ package com.joakimcolloz.stocker.datacollector;
 
 import com.joakimcolloz.stocker.datacollector.data.StockDataService;
 import com.joakimcolloz.stocker.datacollector.data.fetchers.FinanceBirdFetcher;
+import com.joakimcolloz.stocker.datacollector.data.fetchers.YahooFinanceFetcher;
 import com.joakimcolloz.stocker.datacollector.data.parsers.FinanceBirdParser;
+import com.joakimcolloz.stocker.datacollector.data.parsers.YahooFinanceParser;
 import com.joakimcolloz.stocker.datacollector.database.DatabaseConfig;
 import com.joakimcolloz.stocker.datacollector.database.DatabaseManager;
 import com.joakimcolloz.stocker.datacollector.model.Interval;
 import com.joakimcolloz.stocker.datacollector.model.Range;
 import com.joakimcolloz.stocker.datacollector.utils.StockReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,23 +23,52 @@ import java.util.ArrayList;
  * @version 1.1
  */
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String... args) {
         final DatabaseManager databaseManager = new DatabaseManager(new DatabaseConfig());
         databaseManager.initialize();
 
         ArrayList<String> stockList;
 
-        final StockDataService stockDataService = new StockDataService(
-                FinanceBirdParser::new,
-                new FinanceBirdFetcher()
-        );
+        final StockDataService stockDataService = createStockDataService(args);
+
         try {
             stockList = StockReader.readStockNamesFromResource("largecap.txt");
         } catch (IOException e) {
-            System.out.println(e.getMessage()); // TODO temp logging improve!
+            logger.error("Failed to read stock names from resource: {}", e.getMessage());
             throw new RuntimeException(e);
         }
 
         stockDataService.addPriceDataToDb(stockList, Range.THREE_MONTHS, Interval.ONE_DAY);
+    }
+
+    private static StockDataService createStockDataService(String... args) {
+        if (args.length == 0) {
+            logger.info("Using FinanceBird as default to fetch data");
+            return new StockDataService(
+                    FinanceBirdParser::new,
+                    new FinanceBirdFetcher()
+            );
+        } else {
+            final String apiToUse = args[0];
+            switch (apiToUse) {
+                case "FinanceBird" -> {
+                    logger.info("Using FinanceBird to fetch data");
+                    return new StockDataService(
+                        FinanceBirdParser::new,
+                        new FinanceBirdFetcher()
+                    );
+                }
+                case "YahooFinance" -> {
+                    logger.info("Using YahooFinance to fetch data");
+                    return new StockDataService(
+                        YahooFinanceParser::new,
+                        new YahooFinanceFetcher()
+                    );
+                }
+                default -> throw new IllegalArgumentException("Invalid API: " + apiToUse);
+            }
+        }
     }
 }
